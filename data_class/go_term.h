440f55cba1e0228a8d3b21bec49e79d7899f3e70
KochiyaOcean <wyf7789@gmail.com>
2015-06-26 15:55:11 +0800

    fix bugs

--- data_class/go_term.h
+++ data_class/go_term.h
@@ -4,7 +4,15 @@
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
-
+#include <boost/foreach.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/serialization/access.hpp>
+#include <boost/serialization/base_object.hpp>
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/unordered_map.hpp>
 enum LogStatus {SILENT, PART_LOG, FULL_LOG};
 
 class GOTerm {
@@ -14,8 +22,8 @@ public:
 	const int& id() const { return id_; }
 	void set_id(const int& id) { id_ = id; }
 
-	const std::vector<int>& const { return fathers_; }
-	void set_father(const int& fathers) { fathers_ = fathers;  SortFather(); }
+	const std::vector<int>& fathers() const { return childs_; }
+	void set_fathers(const std::vector<int>& fathers) { fathers_ = fathers;  SortFather(); }
 
 	const std::string& term() const { return term_; }
 	void set_term(const std::string& term) { term_ = term; }
@@ -30,7 +38,11 @@ public:
 	void add_child(const int child) { childs_.push_back(child); }
 	void add_father(const int father) { fathers_.push_back(father); }
 private:
-
+	friend boost::serialization::access;
+	template<typename Archive>
+	void serialize(Archive& ar, const unsigned int version) {
+		ar & id_  & term_ & fathers_ & childs_ & type_;
+	}
 
 	void SortChild() {
 		std::sort(childs_.begin(), childs_.end(), [](const int t1, const int t2) { return t1 < t2; });
@@ -49,9 +61,6 @@ private:
 
 class GOTermSet {
 public:
-	GOTypeSet(LogStatus log_status = PART_LOG): update_date_(0), log_status_(log_status) {}
-	GOTypeSet(int update_date, LogStatus log_status = PART_LOG): update_date_(update_date), log_status_(log_status) {}
-
 	std::vector<int> FindAncestors(const std::vector<int>& go_term_ids);
 
 	const GOTerm& QueryGOTerm(int go_id);
@@ -69,9 +78,16 @@ public:
 
 	LogStatus log_status() const { return log_status_; }
 	void set_log_status(LogStatus log_status) { log_status_ = log_status; }
+public:
+	std::unordered_map<int, GOTerm> go_terms_;
+private:
+	friend boost::serialization::access;
+	template<typename Archive>
+	void serialize(Archive& ar, const unsigned int version) {
+		ar & update_date_  & log_status_ & go_terms_;
+	}
 
 private:
-	std::unordered_map<int, GOTerm> go_terms_;
 	/*!@brief yyyymmdd*/
 	int update_date_;
 	LogStatus log_status_;
