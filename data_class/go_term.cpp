aa8c252f16ee9eb0719c37a758f37b8a90cc7dce
KochiyaOcean <wyf7789@gmail.com>
2015-06-26 15:54:04 +0800

    fix bugs

--- data_class/go_term.cpp
+++ data_class/go_term.cpp
@@ -5,17 +5,27 @@
 #include <exception>
 #include <string>
 #include <sstream>
-
+#include <fstream>
+#include <map>
+#include <math.h>
 #include <boost/algorithm/string.hpp>
 #include <boost/archive/binary_iarchive.hpp>
 #include <boost/archive/binary_oarchive.hpp>
 
 using namespace std;
 using namespace boost;
-
-size_t GOTermSet::ParseGoa(const std::string& go_file) {
+size_t GOTermSet::ParseGo(const std::string& go_file) {
 	ifstream fin(go_file);
-  int node = 0;
+	int node = 0;
+	string line;
+	std::map<std::string, int> x;
+	x["id"]=1;
+	x["name"]=2;
+	x["namespace"]=3;
+	x["is_a"]=4;
+	x[" biological_process"]=5;
+	x[" molecular_function"]=6;
+	x[" cellular_component"]=7;
 	while (getline(fin, line)) {
 		/*分割记录*/
 		if (line != ""){
@@ -25,36 +35,36 @@ size_t GOTermSet::ParseGoa(const std::string& go_file) {
       }
       if (line != "[Term]" && line != ""){
         std::vector<string> temp;
-        boost::split( temp, line, boost::is_any_of( ":" ), boost::token_compress_on );
-        switch (temp[0]) {
-          case "id":
+        boost::split(temp, line, boost::is_any_of( ":" ), boost::token_compress_on );
+        switch (x[temp[0]]) {
+          case 1:
             node = atoi(temp[2].c_str());
             go_terms_[node].set_id(node);
             break;
-          case "name":
+          case 2:
             go_terms_[node].set_term(temp[1]);
-          case "namespace":
-            switch temp[1]{
-              case " biological_process"
+          case 3:
+            switch (x[temp[1]]){
+              case 5:
                 go_terms_[node].set_type('P');
                 break;
-              case " molecular_function"
+              case 6:
                 go_terms_[node].set_type('F');
                 break;
-              case " cellular_component"
+              case 7:
                 go_terms_[node].set_type('C');
                 break;
-            }
+        	  }
             break;
-          case "is_a":
+          case 4:
             std::vector<string> temp2;
             boost::split( temp2, temp[2], boost::is_any_of( " " ), boost::token_compress_on );
             int tmpchild = atoi(temp2[0].c_str());
             go_terms_[node].add_child(tmpchild);
             go_terms_[tmpchild].add_father(node);
             break;
-        }
-      }
+         }
+	  }
     }
   }
 }
