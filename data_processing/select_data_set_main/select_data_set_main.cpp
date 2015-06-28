8cb47e00395f45a252554d6e9ed374e8cb300d94
pswgoo <pswgoo@gmail.com>
2015-06-28 10:40:28 +0800

    add data_processing

---
+++ data_processing/select_data_set_main/select_data_set_main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+#include "data_class/protein_sequence.h"
+
+using namespace std;
+
+/**
+ * @brief 可以自己修改接口
+ */
+void SelectTrainSet();
+
+/**
+ * @brief 可以自己修改接口
+ */
+void SelectTestSet();
+
+int main() {
+
+	return 0;
+}
+ 
